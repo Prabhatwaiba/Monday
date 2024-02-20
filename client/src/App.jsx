a8006182190e932e5a09f6f67f26cea41106b716
Prabhatwaiaba <chyangbawaeba@gmail.com>
2024-02-20 19:14:37 +0545

    Routes for pages has been added along with header component is created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,20 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import SignUp from './pages/SignUp'
+import SignOut from './pages/SignOut'
+import Header from './components/Header'
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path='/' element={< Home />}></Route>
+        <Route path='/about' element={<About />}></Route>
+        <Route path='/signup' element={< SignUp />}></Route>
+        <Route path='/signout' element={< SignOut />}></Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
